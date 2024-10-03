78a49ebb4ea425e4da112a19db049688c7c4a421
Aslan Mammadov <a.a.mammdov@xalqsigorta.az>
2024-10-03 22:41:22 +0400

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,11 @@ function App() {
 	const [svgContentTwo, setSvgContentTwo] = useState<string | null | TrustedHTML | undefined>(null);
 	const [targets, setTargets] = useState<Record<any, any>[]>([]);
 	const [count, setCount] = useState(0);
+	const [id, setId] = useState('');
+	const [data, setData] = useState<{ id: number | string | null; isOpen: boolean }>({
+		id: null,
+		isOpen: false,
+	});
 	const [isOpen, setIsOpen] = useState(false);
 	const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const file = event.target.files?.[0];
@@ -33,60 +38,78 @@ function App() {
 		}
 	};
 	const ref = useRef<HTMLDivElement>(null);
-
 	const handleSvgClick = (event: React.MouseEvent<HTMLDivElement>) => {
-		const target = event.target as HTMLElement;
+		const target = event.target as SVGElement;
 		const styles = getComputedStyle(target);
 
 		if (target.tagName !== 'rect' && target.tagName !== 'polygon') {
 			return;
 		}
 
-		setCount((prev) => prev + 1);
+		if (!target.hasAttribute('data-id')) {
+			setCount((prev) => prev + 1);
+			target.setAttribute('data-id', `${count}`);
+			target.setAttribute('data-color', styles.fill!);
+			setTargets((prev) => [
+				...prev,
+				{
+					target: target,
+					index: count,
+				},
+			]);
+		}
+		setData({
+			id: target.getAttribute('data-id'),
+			isOpen: false,
+		});
+		setId(target.getAttribute('data-id') ?? '');
 		setIsOpen(true);
-		setTargets((prev) => [
-			...prev,
-			{
-				target: target,
-				index: count,
-			},
-		]);
-		// if (!isOpen) {
-		// 	target.style.fill = darkenColor(styles.fill!, 20);
-		// } else {
-		// 	target.style.fill = darkenColor(styles.fill!, 100);
-		// }
-		// target.setAttribute(
-		// 	'data',
-		// 	JSON.stringify({
-		// 		some: 'ASS',
-		// 	})
-		// );
-		// target.style.position = 'relative';
-		// const span = document.createElement('span');
-		// span.style.position = 'absolute';
-		// span.style.left = '0';
-		// span.style.top = '0';
 
-		// target.innerHTML = `<span>1<span/>`
+		target.style.fill = darkenColor(styles.fill!, 10);
+		const bbox = (target as SVGRectElement | SVGPolygonElement).getBBox();
+		console.log(bbox, target);
+		const svg = target.parentNode;
+		if (svg) {
+			const existingText = svg.querySelector(`text[data-id="${target.getAttribute('data-id')}"]`);
+			if (existingText) {
+				existingText.remove();
+			}
+
+			const textElement = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+			textElement.setAttribute('x', `${bbox.x + bbox.width / 2}`);
+			textElement.setAttribute('y', `${bbox.y + bbox.height / 2}`);
+
+			textElement.setAttribute('font-size', '22');
+			textElement.setAttribute('fill', 'black');
+			textElement.setAttribute('data-id', `${target.getAttribute('data-id')}`);
+			textElement.textContent = `${target.getAttribute('data-id')}`;
+
+			svg.appendChild(textElement);
+		}
 	};
-
+	const handleSvgClickTwo = (event: React.MouseEvent<HTMLDivElement>) => {
+		const target = event.target as SVGElement;
+		console.log(target)
+	}
 	useEffect(() => {
-		console.log(targets);
-	}, [targets]);
+		const el = ref.current?.querySelector('svg');
+		const e = el?.querySelector(`[data-id="${id}"]`) as SVGAElement;
 
-	// useEffect(() => {
-	// 	console.log(ref.current?.getHTML());
-	// 	setSvgContentTwo(ref.current?.getHTML());
-	// }, [ref, setSvgContentTwo]);
+		if (!isOpen) {
+			if (e) {
+				e.style.fill = e.getAttribute('data-color')!;
+			}
+		}
+	}, [id, ref, isOpen]);
 
 	return (
 		<div className="flex flex-col items-center justify-start bg-slate-300 h-full w-full relative">
 			<input type="file" onChange={onChange} />
 			{isOpen && (
-				<div className="absolute w-full h-full bg-slate-300/50 flex items-center justify-center ">
+				<div className="absolute w-full h-full  flex items-center justify-center ">
 					<div className="absolute  bg-red-400 w-[500px] h-[500px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-						112
+						{data.isOpen && <div>some</div>}
+						{data.id}
 						<button
 							onClick={() => {
 								setIsOpen(false);
@@ -110,13 +133,13 @@ function App() {
 			>
 				button
 			</button>
-			<div className="flex  gap-4 h-screen">
-				<div className="w-[600px] h-[400px] border border-red-400 ">
+			<div className="flex  gap-4 h-screen flex-col w-screen">
+				<div className="w-screen h-full border border-red-400 ">
 					<div ref={ref} dangerouslySetInnerHTML={{ __html: svgContent! }} onClick={handleSvgClick} />
 				</div>
 
-				<div className="w-[600px] h-[400px] border border-red-400">
-					<div dangerouslySetInnerHTML={{ __html: svgContentTwo! }} />
+				<div className="w-screen h-screen border border-red-400">
+					<div dangerouslySetInnerHTML={{ __html: svgContentTwo! }} onClick={handleSvgClickTwo} />
 				</div>
 			</div>
 		</div>
